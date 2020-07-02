bb8b628de164bee9febc1fa2a16668a9b59f69ce
Sandy Rosas <rosassandyboy@gmail.com>
2020-07-02 13:31:26 +0800

    Sandy model

--- game/models.py
+++ game/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from home.models import *
 from .views import *
 from home.views import *
+from django.utils.timezone import now #import timezone
 
 # Create your models here.
 class Result(models.Model):
@@ -36,3 +37,34 @@ class Bet(models.Model):
     def __str__(self):
         return self.choice
 
+#==================== Sandy model structure =======================//
+BET_CHOICES = (
+    ('ODD', 'ODD'),
+    ('EVEN', 'EVEN'),
+)
+
+class Bet_Room(models.Model):
+    room_name = models.CharField(max_length=150,null=False,blank=False)
+    result = models.CharField(choices=BET_CHOICES,max_length=50, null=True,blank=True) #initialize with null
+    participants = models.ManyToManyField(User, through='Bet_Participants') #returns list of participants
+    is_open = models.BooleanField(default=True)
+    is_finish = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.room_name
+
+    def get_result(self):
+        return self.result
+
+class Bet_Participants(models.Model):
+    bet = models.IntegerField(default=0)
+    choice = models.CharField(choices=BET_CHOICES,max_length=50, null=False,blank=False)
+    person = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Bet_Room, on_delete=models.CASCADE)
+    date_participate = models.DateTimeField(default=now)
+
+    def result(self):
+        if self.room.get_result == self.choice:
+            return "WIN"
+        else:
+            return "LOSE"
--- home/models.py
+++ home/models.py
@@ -3,11 +3,10 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Profile(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    user = models.OneToOneField(User,on_delete=models.CASCADE,related_name="profile") # Additional Related name profile
     address = models.TextField(max_length=200,blank=True)
     birthday = models.DateField(null=True, blank=True)
     balance = models.IntegerField(default=100)
 
     def __str__(self):
         return  self.user.username
-
